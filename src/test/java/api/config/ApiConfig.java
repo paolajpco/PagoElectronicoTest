a089011fe4bff413d4d43f515ec53ba19691c74b
paolajpco <paolajpco@gmail.com>
2025-12-12 15:01:29 -0500

    Agregar configuración base en ApiConfig.java

--- src/test/java/api/config/ApiConfig.java
+++ src/test/java/api/config/ApiConfig.java
@@ -1,12 +1,16 @@
 package api.config;
 
 import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import org.junit.jupiter.api.BeforeAll;
 
 public class ApiConfig {
+
     @BeforeAll
     public static void setup() {
-        RestAssured.baseURI = "https://api.example.com";
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.baseURI = "https://reqres.in/api";
+        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
     }
 }
+

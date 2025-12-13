b5b5f12b648d680c743f17b4a44f130f0c0bd349
paolajpco <paolajpco@gmail.com>
2025-12-13 09:30:43 -0500

    Adición Metodos fillInformation,finishPurchase, isPurchaseCompleted a clase CheckoutPage

--- src/test/java/web/pages/CheckoutPage.java
+++ src/test/java/web/pages/CheckoutPage.java
@@ -2,12 +2,29 @@ package web.pages;
 
 import web.config.BasePage;
 
-public class CartPage extends BasePage {
+public class CheckoutPage extends BasePage {
 
-    private final String checkoutButton = "#checkout";
+    private final String firstName = "#first-name";
+    private final String lastName = "#last-name";
+    private final String postalCode = "#postal-code";
+    private final String continueButton = "#continue";
+    private final String finishButton = "#finish";
+    private final String successMessage = ".complete-header";
 
-    public CheckoutPage checkout() {
-        page.click(checkoutButton);
-        return new CheckoutPage();
+    public CheckoutPage fillInformation(String name, String lastNameValue, String zip) {
+        page.fill(firstName, name);
+        page.fill(lastName, lastNameValue);
+        page.fill(postalCode, zip);
+        page.click(continueButton);
+        return this;
+    }
+
+    public CheckoutPage finishPurchase() {
+        page.click(finishButton);
+        return this;
+    }
+
+    public boolean isPurchaseCompleted() {
+        return page.isVisible(successMessage);
     }
 }
